a375c7d4bf716246e1f3dbc30a2af3846031a343
alex-z <blackslayer4@gmail.com>
2023-09-24 14:34:42 +0200

    Moved some drafts to main folder.
    Signed-off-by: alex-z <blackslayer4@gmail.com>

--- drafts/build-WashingMachine-UI-Desktop_Qt_MSVC2019_32bit-Debug/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "D:/Qt/5.15.2/msvc2019/bin/qmake.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "D:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/HostX64/x86/cl.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "D:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/HostX64/x86/cl.exe" CACHE "STRING" "" FORCE)
